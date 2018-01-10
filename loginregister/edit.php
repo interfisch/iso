94cd50adbebe6bcf3e77b4dc441b35a07d9b644f
interfisch <fengfengdestination@hotmail.com>
2018-01-10 23:43:18 +0100

    more field

--- loginregister/edit.php
+++ loginregister/edit.php
@@ -10,14 +10,25 @@ require('includes/config.php');
 if( !$user->is_logged_in() ){ header('Location: /db-interface'); exit(); }
 
 try {
-    $stmt = $db->prepare('SELECT username, memberID, email, institut, address FROM members WHERE memberID = :memberID AND active="Yes" ');
+    $stmt = $db->prepare('SELECT username,password,email,active,institution,`First name`,`Last name`,`Secondary Email`,`Institutional phone`,`Personal phone`,ORCID,`Google scholar profile`,`ResearchGate profile`,`Academia profile`,`Personal website`
+ FROM members WHERE memberID = :memberID AND active="Yes" ');
     $stmt->execute(array('memberID' =>$_SESSION['memberID']));
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
     $old_name = $row['username'];
     $old_email = $row['email'];
-    $old_institut = $row['institut'];
-    $old_address = $row['address'];
+    $old_institution = $row['institution'];
+    $old_firstname = $row['First name'];
+    $old_lastname = $row['Last name'];
+    $old_secondaryemail = $row['Secondary Email'];
+    $old_institutionalphone = $row['Institutional phone'];
+    $old_personalphone = $row['Personal phone'];
+    $old_ORCID = $row['ORCID'];
+    $old_googlescholarprofile = $row['Google scholar profile'];
+    $old_researchGateprofile = $row['ResearchGate profile'];
+    $old_academiaprofile = $row['Academia profile'];
+    $old_personalwebsite = $row['Personal website'];
+
 
 } catch(PDOException $e) {
     echo '<p class="bg-danger">'.$e->getMessage().'</p>';
@@ -29,10 +40,19 @@ if(isset($_POST['submit'])){
     if (!isset($_POST['username'])) $error[] = "Please fill out all fields";
     if (!isset($_POST['email'])) $error[] = "Please fill out all fields";
     if (!isset($_POST['password'])) $error[] = "Please fill out all fields";
-    if (!isset($_POST['institut'])) $error[] = "Please fill out all fields";
-    if (!isset($_POST['address'])) $error[] = "Please fill out all fields";
 
     $username = $_POST['username'];
+    $institution = $_POST['institution'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $secondaryemail = $_POST['secondaryemail'];
+    $institutionalphone = $_POST['institutionalphone'];
+    $personalphone = $_POST['personalphone'];
+    $ORCID = $_POST['ORCID'];
+    $googlescholarprofile = $_POST['googlescholarprofile'];
+    $researchGateprofile = $_POST['researchgateprofile'];
+    $academiaprofile = $_POST['academiaprofile'];
+    $personalwebsite = $_POST['personalwebsite'];
 
     //very basic validation
     if(!$user->isValidUsername($username)){
@@ -61,16 +81,6 @@ if(isset($_POST['submit'])){
     } else {
     }
 
-    $institut = $_POST['institut'];
-    if(strlen($_POST['institut']) < 4){
-        $error[] = 'Please submit the name of your institut.';
-    }
-
-    $address = $_POST['address'];
-    if(strlen($_POST['address']) < 4){
-        $error[] = 'Please submit the address of your institut.';
-    }
-
     //if no errors have been created carry on
     if(!isset($error)){
 
@@ -84,22 +94,48 @@ if(isset($_POST['submit'])){
 
             //Update database with a prepared statement
             if(isset($_POST['editpass'])) {
-                $stmt = $db->prepare('UPDATE members SET password = :password, username = :username, email = :email, institut = :institut, address = :address where memberID = :memberID');
+                $stmt = $db->prepare('UPDATE members SET password = :password, username = :username, email = :email, institution = :institution, 
+                `First name` = :firstname, `Last name` = :lastname, `Secondary Email` = :secondaryemail, `Institutional phone` = :institutionalphone, 
+                `Personal phone` = :personalphone, `ORCID` = :ORCID, `Google scholar profile` = :googlescholarprofile, 
+                `ResearchGate profile` = :researchGateprofile, `Academia profile` = :academiaprofile, `Personal website` = :personalwebsite
+                where memberID = :memberID');
                 $stmt->execute(array(
                     ':password' => $hashedpassword,
                     ':username' => $username,
                     ':email' => $email,
-                    ':institut' => $institut,
-                    ':address' => $address,
+                    ':institution' => $institution,
+                    ':firstname' => $firstname,
+                    ':lastname' => $lastname,
+                    ':secondaryemail' => $secondaryemail,
+                    ':institutionalphone' => $institutionalphone,
+                    ':personalphone' => $personalphone,
+                    ':ORCID' => $ORCID,
+                    ':googlescholarprofile' => $googlescholarprofile,
+                    ':researchGateprofile' => $researchGateprofile,
+                    ':academiaprofile' => $academiaprofile,
+                    ':personalwebsite' => $personalwebsite,
                     ':memberID' => $_SESSION['memberID']
                 ));
             }else{
-                $stmt = $db->prepare('UPDATE members SET username = :username, email = :email, institut = :institut, address = :address where memberID = :memberID');
+                $stmt = $db->prepare('UPDATE members SET username = :username, email = :email, institution = :institution, 
+                `First name` = :firstname, `Last name` = :lastname, `Secondary Email` = :secondaryemail, `Institutional phone` = :institutionalphone, 
+                `Personal phone` = :personalphone, `ORCID` = :ORCID, `Google scholar profile` = :googlescholarprofile, 
+                `ResearchGate profile` = :researchGateprofile, `Academia profile` = :academiaprofile, `Personal website` = :personalwebsite
+                where memberID = :memberID');
                 $stmt->execute(array(
                     ':username' => $username,
                     ':email' => $email,
-                    ':institut' => $institut,
-                    ':address' => $address,
+                    ':institution' => $institution,
+                    ':firstname' => $firstname,
+                    ':lastname' => $lastname,
+                    ':secondaryemail' => $secondaryemail,
+                    ':institutionalphone' => $institutionalphone,
+                    ':personalphone' => $personalphone,
+                    ':ORCID' => $ORCID,
+                    ':googlescholarprofile' => $googlescholarprofile,
+                    ':researchGateprofile' => $researchGateprofile,
+                    ':academiaprofile' => $academiaprofile,
+                    ':personalwebsite' => $personalwebsite,
                     ':memberID' => $_SESSION['memberID']
                 ));
             }
@@ -185,14 +221,50 @@ require('layout/header.php');
                             </div>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <input type="text" name="institut" id="institut" class="form-control input-lg" placeholder="Institut" value="<?php echo $old_institut; ?>" tabindex="6">
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <input type="text" name="firstname" id="firstname" class="form-control input-lg" placeholder="First name" value="<?php echo $old_firstname; ?>" tabindex="6">
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <input type="text" name="lastname" id="lastname" class="form-control input-lg" placeholder="Last name" value="<?php echo $old_lastname; ?>" tabindex="7">
+                            </div>
+                        </div>
                     </div>
                     <div class="form-group">
-                        <input type="text" name="address" id="address" class="form-control input-lg" placeholder="Address" value="<?php echo $old_address ?>" tabindex="7">
+                        <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value="<?php echo $old_email; ?>" tabindex="8">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" name="secondaryemail" id="secondary-email" class="form-control input-lg" placeholder="secondary Email Address" value="<?php echo $old_secondaryemail; ?>" tabindex="9">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="institution" id="institution" class="form-control input-lg" placeholder="Institution" value="<?php echo $old_institution; ?>" tabindex="10">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="institutionalphone" id="institutionalphone" class="form-control input-lg" placeholder="Institutional phone" value="<?php echo $old_institutionalphone; ?>" tabindex="11">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="personalphone" id="personalphone" class="form-control input-lg" placeholder="Personal phone" value="<?php echo $old_personalphone; ?>" tabindex="12">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="ORCID" id="ORCID" class="form-control input-lg" placeholder="ORCID" value="<?php echo $old_ORCID; ?>" tabindex="13">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="googlescholarprofile" id="googlescholarprofile" class="form-control input-lg" placeholder="Google scholar profile" value="<?php echo $old_googlescholarprofile; ?>" tabindex="14">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="researchgateprofile" id="researchgateprofile" class="form-control input-lg" placeholder="ResearchGate profile" value="<?php echo $old_researchGateprofile; ?>" tabindex="15">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="academiaprofile" id="academiaprofile" class="form-control input-lg" placeholder="Academia profile" value="<?php echo $old_academiaprofile; ?>" tabindex="16">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="personalwebsite" id="personalwebsite" class="form-control input-lg" placeholder="Personal website" value="<?php echo $old_personalwebsite; ?>" tabindex="17">
                     </div>
                     <div class="row">
-                        <div class="col-xs-6 col-md-6"><input type="submit" name="submit" value="Edit" class="btn btn-primary btn-block btn-lg" tabindex="8"></div>
+                        <div class="col-xs-6 col-md-6"><input type="submit" name="submit" value="Edit" class="btn btn-primary btn-block btn-lg" tabindex="18"></div>
                         <div class="col-xs-6 col-md-6"><a class="btn btn-primary btn-block btn-lg" href="/db-interface">Cancel</a></div>
                     </div>
                 </form>
--- loginregister/register.php
+++ loginregister/register.php
@@ -6,13 +6,22 @@ if( $user->is_logged_in() ){ header('Location: /db-interface'); exit(); }
 //if form has been submitted process it
 if(isset($_POST['submit'])){
 
-    if (!isset($_POST['username'])) $error[] = "Please fill out all fields";
-    if (!isset($_POST['email'])) $error[] = "Please fill out all fields";
-    if (!isset($_POST['password'])) $error[] = "Please fill out all fields";
-	if (!isset($_POST['institut'])) $error[] = "Please fill out all fields";
-	if (!isset($_POST['address'])) $error[] = "Please fill out all fields";
+    if (!isset($_POST['username'])) $error[] = "Please fill out the username";
+    if (!isset($_POST['email'])) $error[] = "Please fill out the email address";
+    if (!isset($_POST['password'])) $error[] = "Please fill out the password";
 
 	$username = $_POST['username'];
+	$institution = $_POST['institution'];
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$secondaryemail = $_POST['secondaryemail'];
+	$institutionalphone = $_POST['institutionalphone'];
+	$personalphone = $_POST['personalphone'];
+	$ORCID = $_POST['ORCID'];
+	$googlescholarprofile = $_POST['googlescholarprofile'];
+	$researchGateprofile = $_POST['researchgateprofile'];
+	$academiaprofile = $_POST['academiaprofile'];
+	$personalwebsite = $_POST['personalwebsite'];
 
 	//very basic validation
 	if(!$user->isValidUsername($username)){
@@ -55,16 +64,6 @@ if(isset($_POST['submit'])){
 
 	}
 
-	$institut = $_POST['institut'];
-	if(strlen($_POST['institut']) < 4){
-		$error[] = 'Please submit the name of your institut.';
-	}
-
-	$address = $_POST['address'];
-	if(strlen($_POST['address']) < 4){
-		$error[] = 'Please submit the address of your institut.';
-	}
-
 	//if no errors have been created carry on
 	if(!isset($error)){
 
@@ -77,14 +76,24 @@ if(isset($_POST['submit'])){
 		try {
 
 			//insert into database with a prepared statement
-			$stmt = $db->prepare('INSERT INTO members (username,password,email,active,institut,address) VALUES (:username, :password, :email, :active, :institut, :address)');
+			$stmt = $db->prepare("INSERT INTO members (username,password,email,active,institution,`First name`,`Last name`,`Secondary Email`,`Institutional phone`,`Personal phone`,ORCID,`Google scholar profile`,`ResearchGate profile`,`Academia profile`,`Personal website`) 
+			VALUES (:username, :password, :email, :active, :institution, :firstname, :lastname, :secondaryemail, :institutionalphone, :personalphone, :ORCID, :googlescholarprofile, :researchGateprofile, :academiaprofile, :personalwebsite)");
 			$stmt->execute(array(
 				':username' => $username,
 				':password' => $hashedpassword,
 				':email' => $email,
 				':active' => $activasion,
-				':institut' => $institut,
-				':address' => $address
+				':institution' => $institution,
+				':firstname' => $firstname,
+				':lastname' => $lastname,
+				':secondaryemail' => $secondaryemail,
+				':institutionalphone' => $institutionalphone,
+				':personalphone' => $personalphone,
+				':ORCID' => $ORCID,
+				':googlescholarprofile' => $googlescholarprofile,
+				':researchGateprofile' => $researchGateprofile,
+				':academiaprofile' => $academiaprofile,
+				':personalwebsite' => $personalwebsite
 			));
 			$id = $db->lastInsertId('memberID');
 
@@ -150,29 +159,62 @@ require('layout/header.php');
 				<div class="form-group">
 					<input type="text" name="username" id="username" class="form-control input-lg" placeholder="User Name" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['username'], ENT_QUOTES); } ?>" tabindex="1">
 				</div>
-				<div class="form-group">
-					<input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['email'], ENT_QUOTES); } ?>" tabindex="2">
-				</div>
 				<div class="row">
 					<div class="col-xs-6 col-sm-6 col-md-6">
 						<div class="form-group">
-							<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="3">
+							<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="2">
 						</div>
 					</div>
 					<div class="col-xs-6 col-sm-6 col-md-6">
 						<div class="form-group">
-							<input type="password" name="passwordConfirm" id="passwordConfirm" class="form-control input-lg" placeholder="Confirm Password" tabindex="4">
+							<input type="password" name="passwordConfirm" id="passwordConfirm" class="form-control input-lg" placeholder="Confirm Password" tabindex="3">
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<div class="form-group">
+							<input type="text" name="firstname" id="firstname" class="form-control input-lg" placeholder="First name" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['firstname'], ENT_QUOTES); } ?>" tabindex="4">
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<div class="form-group">
+							<input type="text" name="lastname" id="lastname" class="form-control input-lg" placeholder="Last name" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['lastname'], ENT_QUOTES); } ?>" tabindex="5">
 						</div>
 					</div>
 				</div>
 				<div class="form-group">
-					<input type="text" name="institut" id="institut" class="form-control input-lg" placeholder="Institut" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['institut'], ENT_QUOTES); } ?>" tabindex="5">
+					<input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['email'], ENT_QUOTES); } ?>" tabindex="6">
 				</div>
 				<div class="form-group">
-					<input type="text" name="address" id="address" class="form-control input-lg" placeholder="Address" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['address'], ENT_QUOTES); } ?>" tabindex="6">
+					<input type="email" name="secondaryemail" id="secondary-email" class="form-control input-lg" placeholder="secondary Email Address" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['secondaryemail'], ENT_QUOTES); } ?>" tabindex="7">
+				</div>
+				<div class="form-group">
+					<input type="text" name="institution" id="institution" class="form-control input-lg" placeholder="Institution" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['institution'], ENT_QUOTES); } ?>" tabindex="8">
+				</div>
+				<div class="form-group">
+					<input type="text" name="institutionalphone" id="institutionalphone" class="form-control input-lg" placeholder="Institutional phone" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['institutionalphone'], ENT_QUOTES); } ?>" tabindex="9">
+				</div>
+				<div class="form-group">
+					<input type="text" name="personalphone" id="personalphone" class="form-control input-lg" placeholder="Personal phone" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['personalphone'], ENT_QUOTES); } ?>" tabindex="10">
+				</div>
+				<div class="form-group">
+					<input type="text" name="ORCID" id="ORCID" class="form-control input-lg" placeholder="ORCID" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['ORCID'], ENT_QUOTES); } ?>" tabindex="11">
+				</div>
+				<div class="form-group">
+					<input type="text" name="googlescholarprofile" id="googlescholarprofile" class="form-control input-lg" placeholder="Google scholar profile" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['googlescholarprofile'], ENT_QUOTES); } ?>" tabindex="12">
+				</div>
+				<div class="form-group">
+					<input type="text" name="researchgateprofile" id="researchgateprofile" class="form-control input-lg" placeholder="ResearchGate profile" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['researchgateprofile'], ENT_QUOTES); } ?>" tabindex="13">
+				</div>
+				<div class="form-group">
+					<input type="text" name="academiaprofile" id="academiaprofile" class="form-control input-lg" placeholder="Academia profile" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['academiaprofile'], ENT_QUOTES); } ?>" tabindex="14">
+				</div>
+				<div class="form-group">
+					<input type="text" name="personalwebsite" id="personalwebsite" class="form-control input-lg" placeholder="Personal website" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['personalwebsite'], ENT_QUOTES); } ?>" tabindex="15">
 				</div>
 				<div class="row">
-					<div class="col-xs-6 col-md-6"><input type="submit" name="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="7"></div>
+					<div class="col-xs-6 col-md-6"><input type="submit" name="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="16"></div>
 					<div class="col-xs-6 col-md-6"><a class="btn btn-primary btn-block btn-lg" href="/db-interface">Cancel</a></div>
 				</div>
 			</form>
